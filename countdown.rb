09cd7558cddeedff434a25ec43a6a597b9a4b385
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-12 20:56:34 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
+# require_relative './spec_helper'
+# require 'benchmark'
 
-def countdown
+def using_while 
+  def countdown(x = 10)
+  
+    while x > 10
+    countdown = x
+    puts 
+  end
 end
