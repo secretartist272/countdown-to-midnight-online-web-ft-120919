ab861335c9a07b001a93cd74e699e2e7f16637bf
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-15 23:18:30 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,26 @@
+require_relative './spec_helper'
+require 'benchmark'
+require_relative '../countdown.rb'
 
-x = 10
-def countdown(number = x)
- while number > 10
-    puts "#{number} SECOND(S)!"
-    countdown_output(number -= 1)
-    # countdown_with_sleep(5)
-   end
-  # puts "HAPPY NEW YEAR!"
-  "HAPPY NEW YEAR!"
- end
+
+loop do
+  def countdown(number)
+    while number > 0 do
+     puts " #{number} SECOND(S)!"
+     number -= 1
+    end
+    "HAPPY NEW YEAR!"
+  end
+
+  countdown(10)
+
+
+  def countdown_with_sleep(number)
+    while number > 0 do
+     puts " #{number} SECOND(S)!"
+     sleep(5)
+     number -= 1
+    end
+   "HAPPY NEW YEAR!"
+  end
+end
--- spec/countdown_spec.rb
+++ spec/countdown_spec.rb
@@ -1,5 +1,5 @@
-require_relative './spec_helper'
-require 'benchmark'
+# require_relative './spec_helper'
+# require_relative '../countdown.rb'
 
 describe 'LOOPS' do
 
