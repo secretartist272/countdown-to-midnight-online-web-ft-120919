1f84f70f6b307e78374bcca86af06b5356a686c4
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-16 01:13:20 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,20 +1,16 @@
-
-def countdown(number)
-  while number > 0 do
-    puts " #{number} SECOND(S)!"
-    number -= 1
+def countdown(num)
+  while num > 0
+   puts "#{num} SECOND(S)!"
+   num -= 1
+  end
+"HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(num)
+  while num > 0
+   puts "#{num} SECOND(S)!"
+   sleep(1)
+    num -= 1
   end
 "HAPPY NEW YEAR!"
 end
 
-countdown(10)
-
-
-def countdown_with_sleep(number)
-  while number > 0 do
-   puts " #{number} SECOND(S)!"
-   sleep(5)
-   number -= 1
-  end
-"HAPPY NEW YEAR!"
-end
